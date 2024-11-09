a98e22762bc633152fc41fcc5dc16afd950f53d3
christopherjparrett <christopherjparrett@gmail.com>
2024-11-09 02:07:06 -0500

    added seperate mobile page

---
+++ WebsiteReactCode/my-site/src/Pages/mobilePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MobilePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Mobile Page</h1>
+      <p>This page is optimized for mobile devices yet.</p>
+      <p>Please View on computer for optimal experience</p>
+    </div>
+  );
+};
+
+export default MobilePage;
---
+++ WebsiteReactCode/my-site/src/Projects.js
@@ -0,0 +1,62 @@
+//This file contains all of my current projects
+
+const projects = [
+    { 
+      id: 1, 
+      name: "Snake AI", 
+      language: "Python", 
+      codeLink: "https://github.com/user/project-a",
+      icon: "🐍",  // Example icon (you can use any icon library here)
+      description: "Description for project1"
+    },
+    { 
+      id: 2, 
+      name: "Project B", 
+      language: "Python", 
+      codeLink: "https://github.com/user/project-b",
+      icon: "🐍",  // Example icon
+      description: "This is my second project"
+    },
+    { 
+      id: 3, 
+      name: "Project C", 
+      language: "C", 
+      codeLink: "https://github.com/user/project-c",
+      icon: "⚙️",  // Example icon
+      description: "A tool built with JavaScript to automate tasks."
+    },
+    { 
+      id: 4, 
+      name: "Project D", 
+      language: "Java", 
+      codeLink: "https://github.com/user/project-d",
+      icon: "💎",  // Example icon
+      description: "A tool built with JavaScript to automate tasks."
+    },
+    { 
+      id: 5, 
+      name: "Project E", 
+      language: "Python", 
+      codeLink: "https://github.com/user/project-e",
+      icon: "🐍",  // Example icon
+      description: "A tool built with JavaScript to automate tasks."
+    },
+    { 
+      id: 6, 
+      name: "Project F", 
+      language: "Python", 
+      codeLink: "https://github.com/user/project-e",
+      icon: "🐍",  // Example icon
+      description: "A tool built with JavaScript to automate tasks."
+    },
+    { 
+      id: 7, 
+      name: "Project G", 
+      language: "Python", 
+      codeLink: "https://github.com/user/project-e",
+      icon: "🐍",  // Example icon
+      description: "A tool built with JavaScript to automate tasks."
+    }
+  ];
+  
+  export default projects;
